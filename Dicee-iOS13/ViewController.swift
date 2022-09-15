586ca4347d0d4649b13b8cd533e092dd1c548daa
FabrizioPastor <fabriziopastors@outlook.com>
2022-09-15 09:48:51 -0500

    Agregar función roll
    Se agrega la función roll con la cual se obtiene un número al azar y setea el asset correspondiente al UIImage

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var rollButton: UIButton!
     
+    @IBOutlet weak var diceeTwo: UIImageView!
+    @IBOutlet weak var diceeOne: UIImageView!
+    
+    let dicees = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,6 +24,14 @@ class ViewController: UIViewController {
         rollButton.layer.cornerRadius = 12
     }
 
-
+    @IBAction func rollButtonPress(_ sender: UIButton) {        
+        diceeOne.image = UIImage(named: dicees[getRandomNumberForDice()])
+        diceeTwo.image = UIImage(named: dicees[getRandomNumberForDice()])
+    }
+    
+    func getRandomNumberForDice() -> Int {
+        return Int.random(in: 0...5)
+    }
+    
 }
 
