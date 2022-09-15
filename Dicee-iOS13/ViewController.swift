5101a7a38052ae8853a7f1b3ce187b0cae3197d8
FabrizioPastor <fabriziopastors@outlook.com>
2022-09-15 08:40:07 -0500

    Deseñando interfaz

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rollButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        rollButton.layer.cornerRadius = 12
     }
 
 
